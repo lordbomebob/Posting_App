46ff699dc9d4800876bc5b83fe81260d545f1ebd
Arijit Bhardwaj <arijit.bhardwaj@gmail.com>
2024-10-31 19:07:07 -0700

    Added Login and Signup page layout using chakra-ui forms template

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
 import Header from "./components/Header";
+import AuthPage from "./pages/AuthPage";
 
 // Container from chakra-ui wraps our app content in the center
 //Routes for declaring url routes to postpage and profile page
@@ -11,6 +12,7 @@ function App() {
     <Container maxW="620px">
       <Header />
       <Routes>
+        <Route path="/" element={<AuthPage />} />
         <Route path="/:username" element={<UserPage />} />
         <Route path="/:username/post/:pid" element={<PostPage />} />
       </Routes>
---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,13 @@
+import LoginCard from "../components/Login";
+import SignupCard from "../components/SignupCard";
+
+// Authentication Page
+const AuthPage = () => {
+  return (
+    <>
+      <LoginCard />
+    </>
+  );
+};
+
+export default AuthPage;
