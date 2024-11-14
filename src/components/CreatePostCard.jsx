f80326fda1cd5e115ef54e73c8299dbb396ae2e7
lordbomebob <frackwang1107@gmail.com>
2024-11-13 20:13:38 -0800

    form value will update

--- src/components/CreatePostCard.jsx
+++ src/components/CreatePostCard.jsx
@@ -9,13 +9,17 @@ const CreatePostCard = () =>{
     const [imageLinks,setImageLinks]= useState([])
     //add image link to list
     function addImageLink(target){
-        setImageLinks([...imageLinks,{image: target}])  
+        setImageLinks([...imageLinks,{image: target}])
+        //might not work because of delay, might need to use useEffect
+        setPostData({...postData,images:imageLinks})  
     }
     //subtract image link based on index
     function subImageLink(index){
         console.log(imageLinks.length)
         const newImageLinks= imageLinks.filter((_,i)=>i !== index)
         setImageLinks([...newImageLinks])
+        //might not work becuase of delay
+        setPostData({...postData,images:imageLinks})
     }
     //function should add to post component
     function showImage(){
@@ -30,6 +34,22 @@ const CreatePostCard = () =>{
     function clear(){
         setInputImageLink('')
     }
+
+
+    const [postData,setPostData]= useState({
+        userID:'not yet added to find user',
+        title:'',
+        content:''    
+    })
+
+
+    //handle submiting post
+    function handleSubmit(){
+        setPostData({...postData,images:imageLinks})
+        //api post
+        console.log(postData)
+    }
+
     return(
         <Flex align={"center"} justify={"center"} display={"flex"} flexDirection={"column"}>
             <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
@@ -47,25 +67,35 @@ const CreatePostCard = () =>{
                 <Stack >
                     <FormControl isRequired>
                         <FormLabel>Title</FormLabel>
-                        <Input></Input>
+                        <Input placeholder='Enter Title' value={postData.title} onChange={(e)=>setPostData({...postData,title:e.target.value})}></Input>
                     </FormControl>
                     <FormControl>
                         <FormLabel>Content</FormLabel>
-                        <Textarea></Textarea>
+                        <Textarea placeholder="Content" value={postData.content} onChange={(e)=>setPostData({...postData,content:e.target.value})}></Textarea>
                         
                     </FormControl>
                     <FormControl>
-                        <FormLabel>Images</FormLabel>
-                        <Input value={inputImageLink} onChange={handleImageInput}></Input>
+                        <FormLabel>Images Link</FormLabel>
+                        <Input value={inputImageLink} onChange={handleImageInput} marginBottom={3}></Input>
+
                         <Stack display={"flex"} flexDirection={"row"}>
                             <Button color={'green'} onClick={()=> addImageLink(inputImageLink)}>Add Image Link</Button>
                             <Button color={'red'} onClick={()=> clear()}>Clear Link</Button>
                         </Stack>
                     </FormControl>
-                    <Heading>Images Added</Heading>
-                    <ImageList>
+                    <Heading size={'lg'}>Images Added</Heading>
+                    <Heading size={'sm'}>Click Image to Delete</Heading>
+                    <ImageList sx={{
+                                    width: '100%',
+                                    maxHeight: 300,         // Set the height limit for scrolling
+                                    overflowY: 'auto',      // Enable vertical scrolling
+                                    padding: 2,
+                                    border: '1px solid #ddd',
+                                    borderRadius: 2,
+                                    }}>
                             {showImage()}
                     </ImageList>
+                    <Button onClick={()=>handleSubmit()}>Post</Button>
                 </Stack>
             </Box>
         </Flex> 
