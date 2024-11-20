211a1bb03ab616b2d51636c51f7b31a511d92e17
lordbomebob <lordbomebob@gmail.com>
2024-11-19 21:39:50 -0800

    back to default

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { Container } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
-import UserPage from "./pages/UserPage";
-import PostPage from "./pages/PostPage";
 import Header from "./components/Header";
 import AuthPage from "./pages/AuthPage";
-import CreatePostCard from "./components/CreatePostCard";
+import PostPage from "./pages/PostPage";
+import UserPage from "./pages/UserPage";
 
 // Container from chakra-ui wraps our app content in the center
 //Routes for declaring url routes to postpage and profile page
@@ -12,7 +11,6 @@ function App() {
   return (
     <Container maxW="620px">
       <Header />
-      <CreatePostCard></CreatePostCard>
       <Routes>
         <Route path="/" element={<AuthPage />} />
         <Route path="/:username" element={<UserPage />} />
