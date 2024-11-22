6a7d15bae819b5da699ed46f2e12aaf2ff080103
Ian Bajwa <ian.bajwa@gmail.com>
2024-11-22 14:40:28 -0800

    Updated PostPage and PostCard, added SearchPage and Settings .jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,47 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import PostPage from "./pages/PostPage";
 import AuthPage from "./pages/AuthPage";
-import PostPage from "./pages/PostPage";
-import UserPage from "./pages/UserPage";
+import SearchPage from "./pages/Search";
+import Settings from "./pages/Settings";
+import Security from "./pages/Settings/Security";
+import EditProfile from "./pages/Settings/EditProfile";
+import Privacy from "./pages/Settings/Privacy";
 
-// Container from chakra-ui wraps our app content in the center
-//Routes for declaring url routes to postpage and profile page
+// Main App component
 function App() {
   return (
-    <Container maxW="620px">
+    <Flex
+      direction="column"
+      minHeight="100vh" // Ensures the app takes the full height of the viewport
+      bg="black" // Sets the background color for the whole app
+    >
+      {/* Header Component */}
       <Header />
-      <Routes>
-        <Route path="/" element={<AuthPage />} />
-        <Route path="/:username" element={<UserPage />} />
-        <Route path="/:username/post/:pid" element={<PostPage />} />
-      </Routes>
-    </Container>
+
+      {/* Main content area */}
+      <Flex
+        flex="1"
+        justifyContent="center"
+        alignItems="flex-start"
+        bg="black"
+        width="100%"
+      >
+        <Routes>
+          {/* Authentication Page */}
+          <Route path="/" element={<AuthPage />} />
+          {/* User Post Page */}
+          <Route path="/:username/post/:pid" element={<PostPage />} />
+          {/* Search Page */}
+          <Route path="/search" element={<SearchPage />} />
+          {/* Settings Hub Page */}
+          <Route path="/settings" element={<Settings />} />
+          {/* Individual Settings Pages */}
+          <Route path="/settings/security" element={<Security />} />
+          <Route path="/settings/edit-profile" element={<EditProfile />} />
+          <Route path="/settings/privacy" element={<Privacy />} />
+        </Routes>
+      </Flex>
+    </Flex>
   );
 }
 
