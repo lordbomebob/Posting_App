98089c201361ac133ae6a75c68d89df6db3ed201
lordbomebob <lordbomebob@gmail.com>
2024-11-30 03:25:16 -0800

    Create post page added

---
+++ src/pages/CreatePostPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CreatePostCard from '../components/CreatePostCard'
+
+const CreatePostPage = () => {
+  return (
+    <CreatePostCard></CreatePostCard>
+  )
+}
+
+export default CreatePostPage
