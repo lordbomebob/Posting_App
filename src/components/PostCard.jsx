b5059bae717827f5fca8571369b9b5aad8dd0b7d
lordbomebob <lordbomebob@gmail.com>
2024-12-11 17:33:18 -0800

    PostCard instructions

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+/*
+for a single post
+use post.json for json reference
+use single prop
+use useColorModeValue("white", "gray.dark") for color scheme
 
+layout:
+
+Username (small)
+Big title
+content: text
+image Component(add later, leave a space for it)
+tiny time stamp
+[like button, comment button]
+comment component(add later, leave it alone)
+
+*/
 const PostCard = () => {
   return (
     <div className="post-card">
