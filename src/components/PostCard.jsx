3c64c9808d7e7357cf88d522aefa520c5fff12f6
Aaron Huynh <Aaronti2002@gmail.com>
2024-12-12 13:32:37 -0800

    Updated PostCard component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Box, Button, Heading, Text, VStack, HStack, useColorModeValue } from "@chakra-ui/react";
 /*
 for a single post
 use post.json for json reference
@@ -16,13 +17,72 @@ tiny time stamp
 comment component(add later, leave it alone)
 
 */
-const PostCard = () => {
+const PostCard = ({post}) => {
+  const bgColor = useColorModeValue("white", "gray.700");
+  const textColor = useColorModeValue("black", "white");
   return (
-    <div className="post-card">
+    /*<div className="post-card">
       <h3>Post Title</h3>
       <p>This is a brief description of the post.</p>
       <button>Read More</button>
-    </div>
+    </div>*/
+    <Box
+      bg={bgColor}
+      color={textColor}
+      borderRadius="md"
+      p={6}
+      boxShadow="lg"
+      mb={6}
+      maxW="600px"
+      mx="auto"
+    >
+
+      <Text fontSize="sm" fontWeight="bold" mb={2}>
+        {post.username}
+      </Text>
+
+      <Heading size="md" mb={4}>
+        {post.title}
+      </Heading>
+
+      <Text fontSize="md" mb={4}>
+        {post.content}
+      </Text>
+
+      <Box
+        bg="gray.300"
+        height="200px"
+        borderRadius="md"
+        mb={4}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text fontSize="sm" color="gray.600">
+          [Image Placeholder]
+        </Text>
+
+        
+
+       
+   
+
+      </Box>
+
+      <Text fontSize="xs" color="gray.500" mb={4}>
+        {post.timestamp}
+        </Text>
+
+      <HStack spacing={4}>
+        <Button colorScheme="red" size="sm">
+          Like
+        </Button>
+
+        <Button colorScheme="blue" size="sm">
+          Comment
+        </Button>
+      </HStack>
+    </Box>
   );
 };
 
