b3d88ad577eec5c6ea479f7741067100efd6a884
lordbomebob <lordbomebob@gmail.com>
2024-12-11 16:42:52 -0800

    form info from signup, commented out some handle submit, updated form

--- src/components/OnBoardCard.jsx
+++ src/components/OnBoardCard.jsx
@@ -1,15 +1,32 @@
-import { Box, Button, Flex, FormControl, FormLabel, Heading, Image, Input, Stack, Textarea, useColorModeValue } from "@chakra-ui/react";
+import { Box, Button, Flex, FormControl, FormLabel, Heading, HStack, Image, Input, Stack, Textarea, useColorModeValue } from "@chakra-ui/react";
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const OnBoardCard = () => {
+    const navigate = useNavigate()
+    const [userID,setUserID]= useState("token")//userId: "", // Unique ID for the user (Firestore document ID) probally token from local storage
     const [userProfile,setUserProfile]= useState({
-        username:"",
-        profilePic:"",
-        bio:"",
-        userUpvote:[]
+        username: "", // The username of the user
+        fullName: "", // Full name of the user
+        bio: "", // Short biography
+                    // URL of the profile picture
+        profilePicUrl: "https://www.shutterstock.com/image-vector/user-profile-icon-vector-avatar-600nw-2247726673.jpg", 
+        joiningDate: new Date, // Date the user joined
+        posts: [], // Array of post IDs created by this user
+        likedPosts: [], // Array of post IDs liked by this user
+        savedPosts: [], // Array of post IDs saved by this user
+        repliedPosts: [], // Array of post IDs where this user commented on
+        reposts: [] // Array of post IDs that the user reposted
     })
     const handleSubmit = () =>{
-        console.log(userProfile)
+        if(!isValidURL(userProfile.profilePicUrl)){
+            console.warn("invalid url")
+            alert("invalid profile url")
+            return
+        }
+        //upsertUser(userID,userProfile)
+        console.log('handlesubmit after url check')
+        //navigate('/home)
     }
     function isValidURL(url) {
         try {
@@ -34,25 +51,57 @@ const OnBoardCard = () => {
                 w={{ base: "full", sm: "400px" }}
             >
                 <Stack >
-                    <FormControl isRequired>
-                        <FormLabel>Username</FormLabel>
-                        <Input placeholder='Enter Username' value={userProfile.username} onChange={(e)=>setUserProfile({...userProfile,username:e.target.value})}></Input>
-                    </FormControl>
+                    <HStack>
+                        <Box>
+                            <FormControl isRequired>
+                                <FormLabel>Full Name</FormLabel>
+                                    <Input
+                                        type="text"
+                                        value={userProfile.fullName}
+                                        onChange={(e)=>setUserProfile({...userProfile,fullName:e.target.value})}
+                                    />
+                            </FormControl>
+                        </Box>
+                        <Box>
+                            <FormControl isRequired>
+                                <FormLabel>User Name</FormLabel>
+                                <Input
+                                  type="text"
+                                  value={userProfile.userName}
+                                  onChange={(e)=>setUserProfile({...userProfile,userName:e.target.value})}
+                                />
+                            </FormControl>
+                        </Box>
+                    </HStack>
                     <FormControl>
                         <FormLabel>Bio</FormLabel>
                         <Textarea placeholder="Bio" value={userProfile.bio} onChange={(e)=>setUserProfile({...userProfile,bio:e.target.value})}></Textarea>
+                    </FormControl>
+                    <FormControl isRequired>
                         <FormLabel>Profile Picture</FormLabel>
-                        <Input placeholder='Provide link of picture' value={userProfile.profilePic} onChange={(e)=>setUserProfile({...userProfile,profilePic:e.target.value})} marginBottom={3}></Input>
+                        <Input placeholder='Provide link of picture' value={userProfile.profilePicUrl} 
+                            onChange={(e)=>setUserProfile({...userProfile,profilePicUrl:e.target.value})} marginBottom={3}></Input>
+                        <HStack justifyContent={'space-around'}>
+                            <Button 
+                                onClick={()=>setUserProfile({...userProfile,profilePicUrl:"https://www.shutterstock.com/image-vector/user-profile-icon-vector-avatar-600nw-2247726673.jpg"})}
+                                >Default Picture</Button>
+                            <Button 
+                                onClick={()=>setUserProfile({...userProfile,profilePicUrl:""})}
+                                textColor={'red'}
+                                >Clear Link</Button>
+                        </HStack>
                     </FormControl>
                     <Image
-                        src={userProfile.profilePic}
+                        src={userProfile.profilePicUrl}
                         boxSize="150px"
                         borderRadius="full"
                         
                         fit="cover"
                         alt="profile pic"
-                        alignSelf={"center"}></Image>
-                    <Button onClick={()=>handleSubmit()}
+                        alignSelf={"center"}>
+                    </Image>
+                    
+                    <Button onClick={()=>handleSubmit()} //disabled
                         loadingText="Submitting"
                         size="lg"
                         bg={useColorModeValue("gray.600", "gray.700")}
