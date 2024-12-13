b0d9950fc69a0e921fb01ed99f00fcdfc28bbdf1
Aaron Huynh <Aaronti2002@gmail.com>
2024-12-13 01:26:20 -0800

    currently glitched profile query, will fix soon

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProfileComponent from "../components/ProfileComponent";
+import { returnUser } from "../components/returnUser";
+import { getAuth } from "firebase/auth";
 
 const ProfilePage = () => {
-  const profileData = {
+  const [profileData, setProfileData] = useState(null); 
+  const auth = getAuth();
+  const userId = auth.currentUser?.uid;
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const userData = await returnUser(userId);
+        if (userData) {
+          setProfileData(userData);
+        } else {
+          console.error("User not found");
+        }
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+      }
+    };
+
+    if (userId) { 
+      getUserData();
+    }
+  }, [userId]);
+
+   /*const profileData = {
     name: "Example Account",
     username: "ExampleUser",
     bio: "This is just a sample profile :)",
     joinDate: "October 2020",
-  };
+  };*/
+
   return (
     <div>
       <ProfileComponent
