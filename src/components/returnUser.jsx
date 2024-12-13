ce82407b5e15ba28df04a953a0a3234376d396cf
Aaron Huynh <Aaronti2002@gmail.com>
2024-12-13 01:28:21 -0800

    currently glitched profile query, will fix soon

---
+++ src/components/returnUser.jsx
@@ -0,0 +1,19 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig"; 
+
+export const returnUser = async (userId) => {
+  try {
+    const userRef = doc(db, "Users", userId); 
+    const userSnap = await getDoc(userRef);
+
+    if (userSnap.exists()) {
+      return userSnap.data(); 
+    } else {
+      console.error("No such user!");
+      return null; 
+    }
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    throw error; 
+  }
+};
